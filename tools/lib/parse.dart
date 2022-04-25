bf4180c9c238158670bd82a9c111f2566ca91d02
Christopher Fujino <christopherfujino@gmail.com>
2022-04-24 20:11:44 -0700

    wip parser

--- tools/lib/parse.dart
+++ tools/lib/parse.dart
@@ -2,21 +2,152 @@ import 'dart:io' as io;
 
 import 'scanner.dart';
 
-class Config {}
+class Config {
+  const Config(this.declarations);
+
+  final List declarations;
+}
 
 Future<Config> parse(path) async {
   final io.File sourceFile = io.File(path);
   final String source = await sourceFile.readAsString();
   final List<Token> tokenList = await Scanner(source).scan();
-  return await Parser(tokenList).parse();
+  return await Parser(tokenList).parseConfig();
 }
 
 class Parser {
   Parser(this.tokenList);
 
   final List<Token> tokenList;
+  int _index = 0;
+  Token? get _currentToken {
+    if (_index >= tokenList.length) {
+      return null;
+    }
+    return tokenList[_index];
+  }
 
-  Future<Config> parse() async {
-    throw tokenList;
+  Future<Config> parseConfig() async {
+    final List declarations = [];
+    Declaration? currentDeclaration = _parseDeclaration();
+    while (currentDeclaration != null) {
+      declarations.add(currentDeclaration);
+      currentDeclaration = _parseDeclaration();
+    }
+
+    return Config(declarations);
+  }
+
+  Declaration? _parseDeclaration() {
+    final Token? currentToken = _currentToken;
+    if (currentToken == null) {
+      return null;
+    }
+    if (currentToken.type == TokenType.target) {
+      return _parseTargetDeclaration();
+    }
+    throw Exception('Unknown declaration type ${currentToken.type.name}!');
+  }
+
+  /// Parse a [TargetDeclaration].
+  ///
+  /// target_declaration ::= "target", identifier, "{", statement*, "}"
+  TargetDeclaration? _parseTargetDeclaration() {
+    _consume(TokenType.target);
+    final StringToken name = _consume(TokenType.identifier) as StringToken;
+
+    // TODO interesting bits
+    _consume(TokenType.openCurlyBracket);
+    final List<Statement> statements = <Statement>[];
+    Statement? statement = _parseStatement();
+    while (statement != null) {
+      statements.add(statement);
+      statement = _parseStatement();
+    }
+
+    return TargetDeclaration(
+      name: name.value,
+      statements: statements,
+    );
+  }
+
+  Statement? _parseStatement() {
+    Statement? statement = _parseBareStatement();
+    if (statement != null) {
+      return statement;
+    }
+
+    return null;
+  }
+
+  // bare_statement ::= expression, ";"
+  BareStatement? _parseBareStatement() {
+    final Expression? expression = _parseExpression();
+    if (expression == null) {
+      return null;
+    }
+    if (_currentToken!.type != TokenType.semicolon) {
+      return null;
+    }
+    return BareStatement(expression: expression);
+  }
+
+  // Expressions
+
+  Expression? _parseExpression() {
+    Expression? expression = _parseCallExpression();
+    if (expression != null) {
+      return expression;
+    }
+
+    return null;
+  }
+
+  // call_expression ::= identifier, "(", expression?, ")"
+  CallExpression? _parseCallExpression() {
+    final StringToken? name = _consume(TokenType.identifier) as StringToken?;
+    if (name == null) {
+      return null;
+    }
+  }
+
+  /// Consume and return the next token iff it matches [type].
+  ///
+  /// Throws [Exception] if the type is not correct.
+  Token? _consume(TokenType type) {
+    // coerce type as this should only be called if you know what's there.
+    final Token? consumedToken = _currentToken;
+    if (consumedToken == null || consumedToken.type != type) {
+      return null;
+    }
+    _index += 1;
+    return consumedToken;
   }
 }
+
+abstract class Declaration {
+  Declaration({
+    required this.name,
+  });
+
+  final String name;
+}
+
+class TargetDeclaration extends Declaration {
+  TargetDeclaration({
+    required super.name,
+    required this.statements,
+  });
+
+  final Iterable<Statement> statements;
+}
+
+abstract class Statement {}
+
+class BareStatement extends Statement {
+  BareStatement({required this.expression});
+
+  final Expression expression;
+}
+
+abstract class Expression {}
